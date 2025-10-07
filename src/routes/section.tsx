91df7d9cb85cc5cf1f51eafafbaaf03fd48ae513
Marcos Vinicius Reis <marcosreis@somosmds.com>
2025-10-07 18:04:02 -0300

    movendo brands page para o modulo de brands

--- src/routes/section.tsx
+++ src/routes/section.tsx
@@ -8,7 +8,7 @@ import DashboardLayout from '../layout/dashboardLayout';
 
 const SignInPage = lazy(() => import('@/pages/login'));
 const DashboardPage = lazy(() => import('@/pages/DashboardPage'));
-const BrandPage = lazy(() => import('@/pages/BrandsPage'));
+const BrandPage = lazy(() => import('@/modules/brands/BrandsPage'));
 const ProductPage = lazy(() => import('@/pages/ProductsPage'));
 const ServicePage = lazy(() => import('@/pages/ServicePage'));
 const CustomerPage = lazy(() => import('@/pages/CustomerPage'));
