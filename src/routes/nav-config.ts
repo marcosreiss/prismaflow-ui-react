aa8d4c5261ede84ff983139a8b556326a0454235
Marcos Vinicius Reis <m.vinicius.sr11@gmail.com>
2025-10-12 18:24:56 -0300

    --add subitens na sidebar

--- src/routes/nav-config.ts
+++ src/routes/nav-config.ts
@@ -5,9 +5,27 @@ export const navData: NavItem[] = [
   { title: "Marcas", path: "/brands", icon: "Tag" },
   { title: "Produtos", path: "/products", icon: "ShoppingCart" },
   { title: "Serviços", path: "/services", icon: "Box" },
-  { title: "Clientes", path: "/clients", icon: "Users" },
-  { title: "Aniversariantes do dia", path: "/clients-birthday", icon: "Users" },
-  { title: "Receitas Vencidas", path: "/expiring-prescriptions", icon: "Users" },
+  {
+    title: "Clientes",
+    icon: "Users",
+    children: [
+      {
+        title: "Lista de Clientes",
+        path: "/clients",
+        icon: "List",
+      },
+      {
+        title: "Aniversariantes do dia",
+        path: "/clients-birthday",
+        icon: "Cake",
+      },
+      {
+        title: "Receitas Vencidas",
+        path: "/expiring-prescriptions",
+        icon: "AlertTriangle",
+      },
+    ],
+  },
   { title: "Vendas", path: "/sales", icon: "DollarSign" },
-  // { title: "Pagamentos", path: "/payments", icon: "CreditCard" },
+  { title: "Pagamentos", path: "/payments", icon: "CreditCard" },
 ];
